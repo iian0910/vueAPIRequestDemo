c26bf4ced4eb46f3398073cdd4fd3b94fe2dac61
Ian Fan <ian@pollex.com.tw>
2021-01-08 10:05:52 +0800

    fix assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vueAPIRequestDemo/',
+    assetsPublicPath: '/vueAPIRequestDemo/dist/',
 
     /**
      * Source Maps
